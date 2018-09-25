4e351acf294af96b13fa37d0903883f48b54ba8d
Cynda3 <32294372+Cynda3@users.noreply.github.com>
2018-09-25 22:17:02 +0200

    ToLowerCase pending to solve

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 ﻿var fs = require('fs');
 const Telegraf = require('telegraf');
 
+const IS_HEROKU = process.env.API_TOKEN? true: false;
 const API_TOKEN = process.env.API_TOKEN || '';
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
@@ -16,7 +17,7 @@ var raidboss = JSON.parse(fs.readFileSync('raids_pokemon.json','utf-8'));
 bot.hears(/Como .+ a .+ con .+/i, buscar_counter);
 
 function buscar_counter(ctx) {
-	var msg = ctx.message.toLowerCase();
+	var msg = ctx.message;//.toLowerCase();
 	var pokemon = msg.text.split(' a ')[1].split(' con ')[0];
 	var movimiento = msg.text.split(' a ')[1].split(' con ')[1];
 	if (raidboss[pokemon] && raidboss[pokemon][movimiento]) {
@@ -30,6 +31,9 @@ function buscar_counter(ctx) {
 
 bot.start((ctx) => ctx.reply('Welcome to the world of Pokémon!'));
 
-//bot.startPolling();
-bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
-bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
+if (IS_HEROKU) {
+	bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+	bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
+} else {
+	bot.startPolling();
+}
