440c5c368fd3bcf8f722c09e3b8fc2ca4f227e1f
Cynda3 <32294372+Cynda3@users.noreply.github.com>
2018-09-25 22:09:35 +0200

    tolowercase

--- bot.js
+++ bot.js
@@ -5,6 +5,10 @@ const API_TOKEN = process.env.API_TOKEN || '';
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
 
+const STICKERS = {
+	'PERRO_CARA_DE_ASCO': 'CAADAQADbA8AApl_iAIWqjSjvk5mvwI'
+};
+
 const bot = new Telegraf(API_TOKEN);
 
 var raidboss = JSON.parse(fs.readFileSync('raids_pokemon.json','utf-8'));
@@ -12,7 +16,7 @@ var raidboss = JSON.parse(fs.readFileSync('raids_pokemon.json','utf-8'));
 bot.hears(/Como .+ a .+ con .+/i, buscar_counter);
 
 function buscar_counter(ctx) {
-	var msg = ctx.message.toLowercase();
+	var msg = ctx.message.toLowerCase();
 	var pokemon = msg.text.split(' a ')[1].split(' con ')[0];
 	var movimiento = msg.text.split(' a ')[1].split(' con ')[1];
 	if (raidboss[pokemon] && raidboss[pokemon][movimiento]) {
@@ -20,7 +24,7 @@ function buscar_counter(ctx) {
 
 		ctx.reply(`A ${pokemon} se le tiene que tirar con ${counters}`);
 	} else {
-		ctx.replyWithSticker(`CAADAQADbA8AApl_iAIWqjSjvk5mvwI`)
+		ctx.replyWithSticker(STICKERS.PERRO_CARA_DE_ASCO);
 	}
 }
 
