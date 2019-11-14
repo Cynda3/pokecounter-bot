225b059020ded71dab9cac168b3307407b36b590
Cynda3 <32294372+Cynda3@users.noreply.github.com>
2019-11-14 19:57:14 +0100

    Sticker added

--- bot.js
+++ bot.js
@@ -6,8 +6,11 @@ const API_TOKEN = process.env.API_TOKEN || '';
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
 
+
+// Use a Telegram bot to find stickers id
 const STICKERS = {
-	'PERRO_CARA_DE_ASCO': 'CAADAQADbA8AApl_iAIWqjSjvk5mvwI'
+	'PERRO_CARA_DE_ASCO': 'CAADAQADbA8AApl_iAIWqjSjvk5mvwI',
+	'PUTIN': 'CAADAgADuwMAAjq5FQLpT_o8u1zbKhYE'
 };
 
 const bot = new Telegraf(API_TOKEN);
@@ -28,10 +31,12 @@ function buscar_counter(ctx) {
 		ctx.replyWithSticker(STICKERS.PERRO_CARA_DE_ASCO);
 	}
 }
-bot.hears('Oye xabi', oye_xabi);
 
-function oye_xabi(ctx){
-	ctx.reply('No es 100% y con suerte? Triturar.');
+
+bot.hears('Putin', putin);
+
+function putin(ctx){
+	ctx.reply(STICKERS.PUTIN);
 }
 
 bot.start((ctx) => ctx.reply('Welcome to the world of Pokémon!'));
